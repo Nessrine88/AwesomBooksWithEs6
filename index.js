02c237ff353e1c36470f379c6233aa826ed8fce0
Nessrine88 <macherki.nessrine@yahoo.com>
2023-07-18 02:15:29 +0100

    add required changes

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable no-use-before-define */
-
-// eslint-disable-next-line no-unused-vars
-
-// Hidde and display sections
 import {
   addBooks,
   contact,
@@ -11,16 +5,15 @@ import {
   contactHandler,
 } from './modules/hideDisplay.js';
 
-// class declaration
-
 import { BookCollection } from './modules/bookCollection.js';
 
-import { DateTime } from './luxon/build/es6/luxon.js';
+import { DateTime } from './modules/luxon/build/es6/luxon.js';
 
 addBooks.addEventListener('click', addBoo);
 contact.addEventListener('click', contactHandler);
-// eslint-disable-next-line no-unused-vars
+
 const bookCollection = new BookCollection();
+bookCollection.addBook('', '');
 const currentDateElement = document.getElementById('datetime');
 const currentDateTime = DateTime.local();
 currentDateElement.textContent = currentDateTime.toLocaleString(DateTime.DATE_FULL);
